3fd031faf1117671cf3e1b7ed479fea284cd4179
keserarda35 <keserarda35@gmail.com>
2025-12-10 00:30:23 +0300

    add landmarkbook listview solution

---
+++ LandmarkBook/app/src/main/java/com/ardacagankeser/landmarkbook/MainActivity.java
@@ -0,0 +1,67 @@
+package com.ardacagankeser.landmarkbook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ardacagankeser.landmarkbook.databinding.ActivityDetailsBinding;
+import com.ardacagankeser.landmarkbook.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Landmark> landmarks;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        landmarks = new ArrayList<>();
+
+        Landmark pisa = new Landmark("Pisa", "Italy", R.drawable.pisa);
+        Landmark eiffel = new Landmark("Eiffel", "France", R.drawable.eiffel);
+        Landmark colosseum = new Landmark("Colosseum", "Italy", R.drawable.colosseum);
+        Landmark londonBridge = new Landmark("London Bridge", "UK", R.drawable.london_bridge);
+
+        landmarks.add(pisa);
+        landmarks.add(eiffel);
+        landmarks.add(colosseum);
+        landmarks.add(londonBridge);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,
+                landmarks.stream().map(landmark -> landmark.name).collect(Collectors.toList())
+        );
+        binding.listView.setAdapter(arrayAdapter);
+
+        binding.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                intent.putExtra("landmark", landmarks.get(i));
+                startActivity(intent);
+            }
+        });
+    }
+}
